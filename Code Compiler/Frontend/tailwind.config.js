7d1324bed2b7a53be29b0220aa8a2373c3e38c82
Somnath Kaushik <92243973+somnathkaushik@users.noreply.github.com>
2024-04-28 22:34:22 +0530

    linking all the pages properly with node

---
+++ Code Compiler/Frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
